f1588e024ef40cb0e7e2ae7798e1f03f5888036d
hrishikesh <hrishikesh@weboniselab.com>
2018-03-27 12:23:42 +0530

    Recaptcha V2 Integration

--- Model/Behavior/ValidationBehavior.php
+++ Model/Behavior/ValidationBehavior.php
@@ -1,24 +1,26 @@
 <?php
 class ValidationBehavior extends ModelBehavior {
-	function beforeValidate(&$model) {
-		$model->validate['recaptcha_response_field'] = array(
-			'checkRecaptcha' => array(
-				'rule' => array('checkRecaptcha', 'recaptcha_challenge_field'),
-				'required' => true,
-				'allowEmpty' => false,
-				'message' => "Vous n'avez pas tapé les mots correctement. Réessayez s'il vous plaît.",
-			),
-		);
-		return true;
-	}
+    function beforeValidate(&$model)
+    {
+        $model->validate['recaptcha_response_field'] = array(
+            'required' => true,
+            'allowEmpty' => false,
+            'message' => "Recaptcha must be verified"
+        );
+        return true;
+    }
 
-	function checkRecaptcha(&$model, $data, $target) {
-		App::import('Vendor', 'Recaptcha.recaptchalib');
-		$privatekey = Configure::read('Recaptcha.Private');
-		$res = recaptcha_check_answer(
-			$privatekey, 							$_SERVER['REMOTE_ADDR'],
-			$model->data[$model->alias][$target], 	$data['recaptcha_response_field']
-		);
-		return $res->is_valid;
-	}
+    // This method is not required
+    function checkRecaptcha(&$model, $data, $target)
+    {
+        App::import('Vendor', 'Recaptcha.recaptchalib');
+        $privatekey = Configure::read('Recaptcha.Private');
+        $res = recaptcha_check_answer(
+            $privatekey,
+            $_SERVER['REMOTE_ADDR'],
+            $model->data[$model->alias][$target],
+            $data['recaptcha_response_field']
+        );
+        return $res->is_valid;
+    }
 }
